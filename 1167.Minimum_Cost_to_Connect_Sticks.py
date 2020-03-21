b6d5f0d42c32e9791060a84818948d775ade9798
welsny <zengw@mssm.org>
2020-03-21 14:53:37 -0400

    fix syntax

--- 1167.Minimum_Cost_to_Connect_Sticks.py
+++ 1167.Minimum_Cost_to_Connect_Sticks.py
@@ -3,7 +3,7 @@ from heapq import heapify, heappop, heappush
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
         heapify(sticks)
-        
+
         res = 0
         while len(sticks) > 1:
             a, b = heappop(sticks), heappop(sticks)
